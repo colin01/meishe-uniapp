6cdc67d692280210664ce7a002d08ec54b16a002
fly <ck_lin0@163.com>
2026-07-17 14:49:53 +0800

    fix: avoid duplicate Glide app module

--- android-sdkdemo/libBase/src/main/java/com/meishe/base/FJGlideModule.kt
+++ android-sdkdemo/libBase/src/main/java/com/meishe/base/FJGlideModule.kt
@@ -6,7 +6,7 @@ import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.module.LibraryGlideModule
 import java.io.InputStream
 
 /**
@@ -17,7 +17,7 @@ import java.io.InputStream
  * @Copyright :www.meishesdk.com Inc.All rights reserved.
  */
 @GlideModule
-class FJGlideModule : AppGlideModule() {
+class FJGlideModule : LibraryGlideModule() {
     override fun registerComponents(
         context: Context,
         glide: Glide,
